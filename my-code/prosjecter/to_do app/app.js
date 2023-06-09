3681387b4641ffe119e9d8fd0ca90b715fdcec5a
Bunyamin54 <https://github.com/Bunyamin54>
2023-06-09 13:10:17 +0200

    functions

--- my-code/prosjecter/to_do app/app.js
+++ my-code/prosjecter/to_do app/app.js
@@ -1,8 +1,25 @@
 // *** Selector ****
 
 let userInput = document.querySelector("#input-item")
-let userButton = document.querySelector("add-button")
-
+let addButton = document.querySelector("add-button")
+let showTask = document.querySelector(".container")
 
 // *** Event ****
 
+addButton.addEventListener("click", (e) => {
+
+let checkbox = document.createElement("input")
+
+})
+
+function createDiv(){
+
+    let newDiv = document.createElement("div");
+    let newDivChild = document.createElement("div");
+    newDiv.className = "input-group mb-3"
+    newDivChild.className ="input-group-text"
+    newDiv.appendChild(newDivChild)
+
+
+
+}

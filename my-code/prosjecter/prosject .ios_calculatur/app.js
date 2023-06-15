339eee7943701579545a65a2a6bc7ed51674eeda
Bunyamin Genc <bunyaming@uia.no>
2023-06-15 14:17:03 +0200

    border

--- my-code/prosjecter/prosject .ios_calculatur/app.js
+++ my-code/prosjecter/prosject .ios_calculatur/app.js
@@ -1,7 +1,14 @@
-const main =document.querySelector(".grid-container")
+const main = document.querySelector(".grid-container")
+const screen = document.querySelector(".screen")
+
 
 main.addEventListener("click", (e) => {
 
-e.target.innerText
+    let greys = ["0","1","2","3","4","5","6","7","8","9","."]
+    let oranges = ["+","-","X","÷","%"]
 
+  
+    if (greys.includes(e.target.innerText)){
+        screen.innerText += e.target.innerText
+    }
 })
